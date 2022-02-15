9b418fd4108db218c4639784d602dc02fe548c18
sundarik0010 <99658769+sundarik0010@users.noreply.github.com>
2022-02-15 11:34:09 +0530

    Add files via upload

---
+++ LaunchBrowser.java
@@ -0,0 +1,28 @@
+package week2day1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LaunchBrowser {
+	
+
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver= new ChromeDriver();
+		driver.get("http://leaftaps.com/opentaps/control/main");
+		driver.manage().window().maximize();
+		driver.findElement(By.id("username")).sendKeys("Demosalesmanager");
+		driver.findElement(By.id("password")).sendKeys("crmsfa");
+		driver.findElement(By.className("decorativeSubmit")).click();
+        driver.findElement(By.linkText("CRM/SFA")).click();
+        String title=driver.getTitle();
+        System.out.println(title);
+        driver.findElement(By.linkText("Leads"));
+        driver.findElement(By.linkText("create Leads"));
+        
+		
+	}
+
+}
